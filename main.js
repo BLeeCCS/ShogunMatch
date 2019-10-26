c0bd10c86d481155c3e76a738fd78ba7a26a7fda
SuperGato <ledbrandon@yahoo.com>
2019-10-25 20:58:48 -0700

    completed prototype

--- main.js
+++ main.js
@@ -1,1 +1,93 @@
 $(document).ready(intializeApp);
+
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = 0;
+var max_matches = 9;
+var attempts = 0;
+var games_played = 0;
+var image1, image2;
+
+function calculateAccuracy(){
+  var accuracy = matches / attempts;
+  accuracy = accuracy.toFixed(2) * 100;
+  if (isNaN(accuracy)){
+    return 0;
+  }
+  return accuracy;
+}
+function displayStats(){
+  $("aside > div:nth-child(2)").text(games_played);
+  $("aside > div:nth-child(4)").text(attempts);
+  $("aside > div:nth-child(6)").text(calculateAccuracy() + "%");
+}
+function resetStats(){
+  matches = attempts = 0;
+  $(".lfz-card").removeClass("hidden");
+}
+
+function intializeApp() {
+  $(".lfz-card").on("click",handleCardClick);
+}
+
+function handleCardClick(event) {
+  $(event.currentTarget).addClass("hidden");
+  if (firstCardClicked === null) {
+    firstCardClicked = $(event.currentTarget);
+    image1 = firstCardClicked.siblings().css("background-image");
+  } else {
+    secondCardClicked = $(event.currentTarget);
+    image2 = secondCardClicked.siblings().css("background-image");
+
+    ++attempts;
+    $(".lfz-card").off("click", handleCardClick);
+
+    if (image1 !== image2) {
+      setTimeout(function () {
+        firstCardClicked.removeClass("hidden");
+        firstCardClicked = null;
+        $(".lfz-card").on("click", handleCardClick);
+      }, 1500)
+
+      setTimeout(function () {
+        secondCardClicked.removeClass("hidden");
+        secondCardClicked = null;
+      }, 1500)
+
+    } else {
+      firstCardClicked = null;
+      secondCardClicked = null;
+      ++matches;
+      $(".lfz-card").on("click", handleCardClick);
+      if (matches === max_matches) {
+        var modal = {
+          "width":"40vw",
+          "height": "20vh",
+          "background-color":'black',
+          "background-size":"cover",
+          "position":"absolute",
+          "display":"inline-block",
+          "margin":"auto",
+          "z-index":"2",
+          "text-align":"center",
+          "font-size":"3rem",
+          "padding":"20vh",
+          "border":"5px solid red",
+          "color": "yellow"
+        }
+        var myModal = $("<div>").css(modal).text("Congratulations!You Won!");
+        var button = $("<button>").css("font-size","2rem").text("Play Again?");
+        myModal.addClass("modalClass");
+        button.addClass("myButton");
+        button.appendTo(myModal);
+        $("body").append(myModal);
+        ++games_played;
+        $(".myButton").on("click",function() {
+          $(".modalClass").hide();
+        })
+        resetStats();
+      }
+    }
+  }
+  displayStats();
+}
